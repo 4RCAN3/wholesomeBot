682687fb22f11d0208a7df2a19115cc3fff7aa56
Devansh3712 <devanshamity@gmail.com>
2020-09-12 16:09:42 +0530

    updated memes

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import random
 from discord.ext import commands
 
 TOKEN = open("token.txt","r").readline()
-client = commands.Bot(command_prefix = '!')
+client = commands.Bot(command_prefix = '~')
 
 client.remove_command('help')
 
@@ -12,6 +12,11 @@ async def wholesome(ctx):
     responses = open('quotes.txt').read().splitlines()
     random.seed(a=None)
     response = random.choice(responses)
+    await ctx.send(response)
+
+@client.command()
+async def coco(ctx):
+    response = "You are the most perfect human there is to exist in history!"
     await ctx.send(response)
 
 @client.command()
@@ -30,20 +35,20 @@ async def ily(ctx):
 
 @client.command()
 async def qt(ctx):
-    await ctx.send(file=discord.File('./memes/14.jpg'))
+    await ctx.send(file=discord.File('./memes/meme (40).jpg'))
 
 @client.event
 async def on_command_error(ctx, error):
-    await ctx.send(f'({error}), use !help for usage')
+    await ctx.send(f'({error}), use ~help for usage')
 
 @client.command(pass_context=True)
 async def help(ctx):
     embed = discord.Embed(
         colour = discord.Colour.green())
     embed.set_author(name='Help : list of commands available')
-    embed.add_field(name='!wholesome', value='Responds with a wholesome quote :)', inline=False)
-    embed.add_field(name='!meme', value='Posts a random whomesome meme <3', inline=False)
-    embed.add_field(name='!all', value='List of all commands', inline=False)
+    embed.add_field(name='~wholesome', value='Responds with a wholesome quote :)', inline=False)
+    embed.add_field(name='~meme', value='Posts a random whomesome meme <3', inline=False)
+    embed.add_field(name='~all', value='List of all commands', inline=False)
     await ctx.send(embed=embed)
 
 @client.command(pass_context=True)
@@ -51,8 +56,9 @@ async def all(ctx):
     embed = discord.Embed(
         colour = discord.Colour.red())
     embed.set_author(name='All commands of wholesomeBot')
-    embed.add_field(name='!qt', value='Responds with you\'re a cutie meme ;)', inline=False)
-    embed.add_field(name='!ily', value='Responds with an ily message <3', inline=False)
+    embed.add_field(name='~qt', value='Responds with you\'re a cutie meme ;)', inline=False)
+    embed.add_field(name='~ily', value='Responds with an ily message <3', inline=False)
+    embed.add_field(name='~coco', value='Responds with something special ;)', inline=False)
     await ctx.send(embed=embed)
 
 client.run(TOKEN)
