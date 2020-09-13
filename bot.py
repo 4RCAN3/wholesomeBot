e46409144545d18155689d914ef4357b65fcd0df
Devansh3712 <devanshamity@gmail.com>
2020-09-13 10:34:07 +0530

    updates

--- bot.py
+++ bot.py
@@ -52,6 +52,13 @@ async def cheerup(ctx):
     await ctx.send(file=discord.File(response))
 
 @client.command()
+async def homie(ctx):
+    responses = open('homies.txt').read().splitlines()
+    random.seed(a=None)
+    response = './homies/'+random.choice(responses)
+    await ctx.send(file=discord.File(response))
+
+@client.command()
 async def ily(ctx):
     responses = open('ily.txt').read().splitlines()
     random.seed(a=None)
@@ -65,6 +72,14 @@ async def qt(ctx):
 @client.command()
 async def noU(ctx):
     await ctx.send(file=discord.File('./love/noU.jpg'))
+
+@client.command()
+async def bb(ctx):
+    await ctx.send(file=discord.File('./cheerup/bb.jpg'))
+
+@client.command()
+async def ohh(ctx):
+    await ctx.send(file=discord.File('./love/bestf.png'))
 
 @client.event
 async def on_command_error(ctx, error):
@@ -96,6 +111,9 @@ async def all(ctx):
     embed.add_field(name='~love', value='Responds with a love meme ;)', inline=False)
     embed.add_field(name='~cheerup', value='Responds with a cheerup meme <3', inline=False)
     embed.add_field(name='~noU', value='Responds with a uno reverse card :)', inline=False)
+    embed.add_field(name='~homie', value='Responds with a homies meme <3', inline=False)
+    embed.add_field(name='~bb', value='For the office fans ;)', inline=False)
+    embed.add_field(name='~ohh', value='Remember the yellow sunglasses guy?', inline=False)
     await ctx.send(embed=embed)
 
 @client.command(pass_context=True)
@@ -103,10 +121,10 @@ async def info(ctx):
     embed = discord.Embed(
         colour = discord.Colour.purple())
     embed.set_author(name='Information')
-    embed.add_field(name='~info', value='Gives info about the wholesomeBot', inline=False)
-    embed.add_field(name='~wholesome', value='Responds with a wholesome quote :)', inline=False)
-    embed.add_field(name='~meme', value='Posts a random whomesome meme <3', inline=False)
-    embed.add_field(name='~all', value='List of all commands', inline=False)
+    embed.add_field(name='What is it?', value='wholesomeBot is a discord bot made using `python` for wholesome content ;)', inline=False)
+    embed.add_field(name='Hosting', value='Hosted on Heroku, currently in development stage', inline=False)
+    embed.add_field(name='Author', value='Made with *uWu* by `Devansh`', inline=False)
+    embed.add_field(name='Visit', value='https://devansh3712.github.io/wholesomeBot', inline=False)
     await ctx.send(embed=embed)
 
 client.run(TOKEN)
